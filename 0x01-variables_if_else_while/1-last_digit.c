80d205a1de8ad3dc7f9d4d6cba11155934f122b5
kingmajor2k19 <mysylvabird.com>
2022-09-09 22:38:13 -0700

    digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- * return: Always 0
+ * main - Prints the last digits of a randomly generated number and whether it is greater thn 5, less than 6, or 0.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
