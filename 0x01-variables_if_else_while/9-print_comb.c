8929cf5b177027cc75704d4575e8b792ccbcbb61
kingmajor2k19 <mysylvabird.com>
2022-09-09 22:01:50 -0700

    last commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+int num;
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+if (num != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
