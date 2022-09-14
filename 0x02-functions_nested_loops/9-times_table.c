553cdf1dda9d42edabadd49eb0a60061daa03408
kingmajor2k19 <mysylvabird.com>
2022-09-13 21:50:13 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,14 +15,12 @@ void times_table(void)
 	{
 		_putchar(',');
 		_putchar(' ');
-		
 		prod = num * mult;
 
 		if (prod <= 9)
 			_putchar(' ');
 		else
 			_putchar((prod / 10) + '0');
-		
 		_putchar((prod % 10) + '0');
 	}
 	_putchar('\n');
