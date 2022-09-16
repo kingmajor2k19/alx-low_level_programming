b58a25b30cba51500c48cbee4b7881881fd1658e
kingmajor2k19 <mysylvabird.com>
2022-09-15 20:35:16 -0700

    pnc

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,22 +2,47 @@
 #include "main.h"
 
 /**
-* print_number - Prints a number
-* @n: The number to print
+* print_number -  checks for checks for a digit (0 through 9).
+* @n: n -  Variable
+* Return: Always 0.
 */
-
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int z;
+	int m, b;
 
-	if (n < 0)
+	b = 10;
+
+	if (n < 10 && n >= 0)
+	
 	{
-		putchar('-');
-		num = -num;
+		_putchar (n + '0');
 	}
-	if (num > 9)
+	else if (n > -10 && n < 0)
 	{
-		print_number(num / 10);
+		n = n - 2 * n;
+		_putchar('-');
+		_putchar (n + '0');
 	}
-	putchar(num % 10 + '0');
+
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1
+			_putchar ('-');
+		}
+		z = n;
+	while (z / b > 9)
+	{
+		b = b * 10;
+	}
+	while (b > 0)
+	{
+		m = z / b;
+		z = z % b;
+		_putchar (m + '0');
+		b = b / 10;
+	}
+	}
 }
