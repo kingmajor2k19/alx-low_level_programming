0b794adbb361891ba8340543099276157457473f
kingmajor2k19 <mysylvabird.com>
2022-09-19 14:11:48 -0700

    7 commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
- * @str: input
- * Return: half of input
- */
+* puts_half - a function that prints half of a string
+* if odd len, n = (length_of_the_string - 1) / 2
+* @str: input
+* Return: half of input
+*/
 
 void puts_half(char *str)
 {
