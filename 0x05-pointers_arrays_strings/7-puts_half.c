b5920382159476de15be5093ab7d1caba48b07a8
kingmajor2k19 <mysylvabird.com>
2022-09-20 05:23:29 -0700

    seven commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,24 +1,26 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
-*puts_half - function that  prints half of a string,
-*@str: first value to check
-*
+* puts_half - a function that prints half of a string
+* if odd len, n = (length_of_the_string - 1) / 2
+* @str: input
+* Return: half of input
 */
 void puts_half(char *str)
 {
-	int i = 0, k, j;
+	int i, n, len;
 
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	if (i % 2 == 0)
-		j = i / 2;
-	else
-		j = ((i - 1) / 2) + 1;
-	for (k = j; k < i;  k++)
-	{
-		_putchar(str[k])
-	}
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	n = (len / 2);
+
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
