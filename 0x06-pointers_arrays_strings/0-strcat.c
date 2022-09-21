575d1d0379e244085248f7a21be87faa83f77dfb
kingmajor2k19 <mysylvabird.com>
2022-09-21 08:20:48 -0700

    concatenates strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,10 @@ char *_strcat(char *dest, char *src)
 
 	int index = 0, dest_len = 0;
 
-	while(dest[index++])
+	while (dest[index++])
 		dest_len++;
-	
-	for(index = 0; src[index]; index++)
+
+	for (index = 0; src[index]; index++)
 		[dest_len++] = src[index];
 
 	return (dest);
