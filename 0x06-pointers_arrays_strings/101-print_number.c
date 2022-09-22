7379428076c24a0f009fd2675ba559d2576be9a7
kingmajor2k19 <mysylvabird.com>
2022-09-21 20:03:53 -0700

    print int

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,15 @@
  */
 void print_number(int n)
 {
-	unsigned int num;
+	unsigned int num = n;
 /*check if number is negative*/
-	num = n;
 	if (n < 0)
 	{
-		_putchar(45);
+		_putchar('_');
 		num = -n;
 	}
 /* print number by recursion*/
-	if (num / 10)
-	{
+	if ((num / 10) > 0)
 		print_number(num / 10);
-	}
 	_putchar((num % 10) + '0');
 }
