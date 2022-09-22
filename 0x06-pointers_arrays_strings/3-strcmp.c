18d438a865299c079e43c9dd62bdc51f615f1efe
kingmajor2k19 <mysylvabird.com>
2022-09-22 07:18:27 -0700

    scramp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,40 +1,15 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * _strcmp - functions which compares two strings and
- * @s1: first string
- * @s2: second string
- * Return:
- *		returns zero if s1 == s2
- *		returns negative number if s1 < s2
- *		returns positive number if s1 > s2
+ * _strcmp -> for comparing purpose
+ * @s1: param1
+ * @s2: param2
+ * Return: integer
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, diff = 0;
+	int result;
 
-	while (1)
-	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-			break;
-		else if (s1[i] == '\0')
-		{
-			diff = s2[i];
-			break;
-		}
-		else if (s2[i] == '\0')
-		{
-			diff = s1[i];
-			break;
-		}
-		else if (s1[i] != s2[i])
-		{
-			diff = si[i] - s2[i];
-			break;
-		}
-		else
-			i++;
-	}
-	return (diff);
+	result = strcmp(s1, s2);
+	return (result);
 }
