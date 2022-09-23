242778c4561ded0194cf6b7d6a5664bfac156e02
kingmajor2k19 <mysylvabird.com>
2022-09-22 20:26:46 -0700

    magic moment

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-/*
- * main - add one line of code to print a[2] = 98
- *
- * Return: 0
- */
+
 int main(void)
 {
 	int n;
@@ -13,7 +9,8 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of code here... remeber:
+	 * write your line of code here...
+	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
