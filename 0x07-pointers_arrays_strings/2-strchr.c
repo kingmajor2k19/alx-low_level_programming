e517902aad5bccb18f000d53836bbe008ee03130
kingmajor2k19 <mysylvabird.com>
2022-09-26 14:17:20 -0700

    hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,18 +8,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int a;
+	int i;
 
-	while (1)
+	for (i = 0; *(s + i); i++)
 	{
-		a = *s++;
-		if (a == c)
-		{
-			return (s - 1);
-		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
+		if (*(s + i) == c)
+			return (s + i);
 	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
 }
