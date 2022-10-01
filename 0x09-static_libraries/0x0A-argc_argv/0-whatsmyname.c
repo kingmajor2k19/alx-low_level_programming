55b8927cfc86f7f011433479d3dffcdc9d0a8d87
kingmajor2k19 <mysylvabird.com>
2022-10-01 07:43:14 -0700

    hot fix

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is a function that prints its name
+ * @argc: argument count
+ * @argv: an array of pointers to argument string
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
