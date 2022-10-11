121a5bacafe39dd5a8dcc815045bddc68f932629
kingmajor2k19 <mysylvabird.com>
2022-10-10 21:48:49 -0700

    macro fun

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
 
-#define ABS(x) ((x) < (0) ? (-x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
