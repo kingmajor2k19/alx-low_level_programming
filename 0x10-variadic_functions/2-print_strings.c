1ffe0979904331d55de4a4db171e1e2f8f06724f
kingmajor2k19 <mysylvabird.com>
2022-10-13 21:21:20 -0700

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - print strings
+ * @separator: string between the strings
+ * @n: number of parameters received
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+
+	va_list strings;
+
+	va_start(strings, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		str =  va_arg(strings, char *);
+
+		if (str == NULL)
+		{
+			printf("%p", str);
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(strings);
+	printf("\n");
+}
