5dca8adf4fccdf5cea87c4ccf9271c9b831bc993
kingmajor2k19 <stephentimothy657@gmail.com>
2022-12-14 01:36:56 -0800

    dynamic

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * on success: returns no error
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
